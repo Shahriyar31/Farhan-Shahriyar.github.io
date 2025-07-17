14f45c6e5002a5a1efab5dd4d8941db06c02866c
Shahriyar31 <shahriyarfarhan3101@gmail.com>
2025-07-17 21:17:06 +0200

    Fix Astro base path for GitHub Pages trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Shahriyar31.github.io",
-  base: "/Farhan-Shahriyar.github.io",
+  base: "/Farhan-Shahriyar.github.io/", // <--- Add the trailing slash here
   integrations: [tailwind()]
 });
