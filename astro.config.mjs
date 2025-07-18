c0a1ee5e367eaa8245cca0a6d12dc79c1506f551
Shahriyar31 <shahriyarfarhan3101@gmail.com>
2025-07-18 10:27:21 +0200

    feat: Add GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Shahriyar31.github.io",
-  base: "/Farhan-Shahriyar.github.io/", // <--- Add the trailing slash here
+  // The `site` property should be the full URL of your project.
+  site: "https://Shahriyar31.github.io/Farhan-Shahriyar.github.io",
+  // The `base` property should be the name of your repository.
+  base: "/Farhan-Shahriyar.github.io/", 
   integrations: [tailwind()]
 });
